ebc355d974e4d46dbec8a1b898e3803ca9b8185a
sanyogpatel-tecblic <sanyogpatel.tecblic@gmail.com>
2024-09-18 12:41:38 +0530

    Stopped tracking env.local, and env.staging

--- apps/users_upload/views.py
+++ apps/users_upload/views.py
@@ -1,15 +1,15 @@
-# views.py (in your Django app)
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.files.storage import default_storage
-from .tasks import process_csv
+# # views.py (in your Django app)
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from django.core.files.storage import default_storage
+# from .tasks import process_csv
 
-class FileUploadView(APIView):
-    authentication_classes=[]
-    permission_classes=[]
-    def post(self, request, *args, **kwargs):
-        file = request.FILES['file']
-        file_path = default_storage.save(f'uploads/{file.name}', file)
-        process_csv.delay(file_path)  
-        return Response({"message": "File uploaded successfully, processing started."}, status=status.HTTP_201_CREATED)
+# class FileUploadView(APIView):
+#     authentication_classes=[]
+#     permission_classes=[]
+#     def post(self, request, *args, **kwargs):
+#         file = request.FILES['file']
+#         file_path = default_storage.save(f'uploads/{file.name}', file)
+#         process_csv.delay(file_path)  
+#         return Response({"message": "File uploaded successfully, processing started."}, status=status.HTTP_201_CREATED)
