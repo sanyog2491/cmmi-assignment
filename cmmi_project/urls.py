0ff2d6691d13649751ef3bb8930d28235347e139
sanyog2491 <sanyogpatel2491999@gmail.com>
2024-09-19 18:40:33 +0000

    added

--- cmmi_project/urls.py
+++ cmmi_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib import admin
+admin.site.site_header = "CMMI Administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.accounts.urls')),  
